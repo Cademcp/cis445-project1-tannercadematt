386d490f8db4bf75995d4acf4f6d47fb2d2c1aa6
tannergraham1 <tgraham@mail.bradley.edu>
2018-10-03 11:09:04 -0500

    first commit-practice

---
+++ HelloWorld.py
@@ -0,0 +1,33 @@
+print("Hello World!")
+x = 5
+y = 10
+if x < y:
+    print(x + y)
+testList = ["first", "second", "third"]
+print(testList)
+print(testList[2])
+if x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
+else:
+    print("x is less than y")
+i = 0
+while i < 6:
+    print(i)
+    i += 1
+
+for x in testList:
+    print(x)
+
+
+def my_function(name):
+    print("Hello my name is " + name)
+
+
+my_function("Tanner")
+
+
+
+
+
---
+++ testFlaskApp.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "This is the homepage"
+
+
+if __name__ == "__testFlaskApp__":
+    app.run(debug=True)
+
+
+
