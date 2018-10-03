e6d0cc84d093e4e84819332e7187cb56624eb166
tannergraham1 <tgraham@mail.bradley.edu>
2018-10-03 18:02:08 -0500

    Passing data to template

--- CODE/testFlaskApp.py
+++ CODE/testFlaskApp.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+
+testDB = {
+    "Player": {
+        "Name": "Carlos Gonzalez",
+        "Number": "22",
+        "Position": "OF",
+        "Age": "27",
+        "Hits": "L",
+        "Throws": "L",
+        "Image": "Ap_Creative_Stock_Header.jpg"
+    }
+
+
+}
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "This is the homepage"
+    name = testDB['Player']['Name']
+    number = testDB['Player']['Number']
+    position = testDB['Player']['Position']
+    age = testDB['Player']['Age']
+    hits = testDB['Player']['Hits']
+    throws = testDB['Player']['Throws']
+    image = testDB['Player']['Image']
+    return render_template(index.html, playerName=name, playerNumber=number, playerPosition=position, playerAge=age,
+                           playerHits=hits, playerThrows=throws, playerImage=image)
 
 
 if __name__ == "__main__":
