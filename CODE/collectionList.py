0b66c0d07ad2dac44b1ffe9d43244406fea53ae0
Matt Gates <mgates@mail.bradley.edu>
2018-10-10 12:52:52 -0500

    Added all 4 teams to the database and included a variable to distinguish between them. New code prints the roster for each team and the team name

--- CODE/collectionList.py
+++ CODE/collectionList.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
 import pymongo
 
-client = pymongo.MongoClient('mongodb+srv://mattgates:passwordmdb@tannercadematt-kkd3l.mongodb.net/test?retryWrites=true')
+client = pymongo.MongoClient(
+    'mongodb+srv://mattgates:passwordmdb@tannercadematt-kkd3l.mongodb.net/test?retryWrites=true')
 
 
 db = client.Project1
@@ -9,9 +9,34 @@ db = client.Project1
 collection = db.Project1
 
 rockies = []
+dodgers = []
+braves = []
+brewers = []
 
-for x in collection.find():
+print("=========================================Rockies")
+for x in collection.find({"Team": "Rockies"}):
     rockies.append(x)
 
 for x in rockies:
+    print(x['Player Name'])
+
+print("=========================================Dodgers")
+for x in collection.find({"Team": "Dodgers"}):
+    dodgers.append(x)
+
+for x in dodgers:
+    print(x['Player Name'])
+
+print("=========================================Braves")
+for x in collection.find({"Team": "Braves"}):
+    braves.append(x)
+
+for x in braves:
+    print(x['Player Name'])
+
+print("=========================================Brewers")
+for x in collection.find({"Team": "Brewers"}):
+    brewers.append(x)
+
+for x in brewers:
     print(x['Player Name'])
